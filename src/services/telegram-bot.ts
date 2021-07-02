3b9d335654d9f11261f5bb4b42861236a845685e
noxend <noxios5674@gmail.com>
2021-07-02 16:46:12 +0300

    fix

--- src/services/telegram-bot.ts
+++ src/services/telegram-bot.ts
@@ -2,8 +2,8 @@ import TelegramBot from "node-telegram-bot-api";
 
 import config from "@/config";
 
-if (!config.TELEGRAM_TOKEN) throw new Error("Telegram token not provided");
+if (!config.TELEGRAM_BOT_TOKEN) throw new Error("Telegram token not provided");
 
-const bot = new TelegramBot(config.TELEGRAM_TOKEN, { polling: true });
+const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, { polling: true });
 
 export default bot;
