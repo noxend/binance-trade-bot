4c9d5c6c8001cab1a4421be8bd8f1097a8414283
noxend <noxios5674@gmail.com>
2021-07-03 15:09:52 +0300

    minor changes

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import binance from "./services/binance-api";
-
-binance.futuresStartDataStream().then(({ listenKey }) => {
-  binance.futuresSubscribeToUpdates(listenKey, (update) => {
-    update.e === 'ORDER_TRADE_UPDATE'
-  });
-});
